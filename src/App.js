1c3b22bb993121455d9c8b2c9bbc12d0d58e8952
bbrooksy <72233725+bbrooksy@users.noreply.github.com>
2021-05-16 20:45:19 -0400

    Styling Added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useState } from 'react';
 import "./App.css";
 import Body from './Components/Body'
 import Head from './Components/Head'
-import axios from 'axios'
+
+
+import { Nav, NavItem, NavLink } from 'reactstrap';
+
+const HeadNav = (props) => {
+  return (
+    <div>
+      <hr />
+      <p>Learn More</p>
+      <Nav>
+      <NavLink href="#">Home</NavLink> 
+      <NavLink href="#">Photos</NavLink>
+      <NavLink href="#">Explore</NavLink> 
+      <NavLink disabled href="#">Contact</NavLink>
+      </Nav>
+    </div>
+  );
+}
+
+  
+
 
 function App() {
   return (
     <div className="App">
       <p>
       <Head />
+      <p>
+      <HeadNav />
+      </p>
       <Body />
       </p>
     </div>
@@ -16,3 +39,4 @@ function App() {
 }
 
 export default App;
+
